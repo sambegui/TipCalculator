0945842d02c312260ae4ff84c5d90ba3c9251c54
Samuel Beguiristain <info@begui.xyz>
2023-10-01 23:17:58 -0400

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Samuel Beguiristain on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
